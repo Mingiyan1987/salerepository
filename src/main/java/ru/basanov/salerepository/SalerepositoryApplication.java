2c5baa10f30d758b18dd0ab40a7e3c91ecd805c2
Mingiyan1987 <basanov.mt87@gmail.com>
2019-12-06 01:05:11 +0300

    Тестовое задание

---
+++ src/main/java/ru/basanov/salerepository/SalerepositoryApplication.java
@@ -0,0 +1,13 @@
+package ru.basanov.salerepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalerepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalerepositoryApplication.class, args);
+	}
+
+}
